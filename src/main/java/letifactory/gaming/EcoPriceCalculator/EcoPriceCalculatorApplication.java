7f72e6ad2df699328590d97fd0aed0ac126f26b5
Letimineur <seb.colin91@gmail.com>
2023-08-28 12:16:24 +0200

    Init from spring initializr

---
+++ src/main/java/letifactory/gaming/EcoPriceCalculator/EcoPriceCalculatorApplication.java
@@ -0,0 +1,13 @@
+package letifactory.gaming.EcoPriceCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoPriceCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoPriceCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/letifactory/gaming/EcoPriceCalculator/EcoPriceCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package letifactory.gaming.EcoPriceCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoPriceCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
